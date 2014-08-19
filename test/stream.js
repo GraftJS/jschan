d6ce552030e6716ba192f960c16535180fc17045
Matteo Collina <matteo.collina@gmail.com>
2014-08-19 17:18:10 +0200

    no header support for streams.

--- test/stream.js
+++ test/stream.js
@@ -5,12 +5,22 @@ var jschan          = require('../lib/jschan');
 var abstractSession = require('./abstract_session');
 var PassThrough     = require('readable-stream').PassThrough;
 
-describe('stream session', function() {
+describe('stream session with standard mode', function() {
   abstractSession(function(cb) {
     var inStream  = new PassThrough();
     var outStream = new PassThrough();
     var inSession = jschan.streamSession(inStream, outStream, { server: true });
     var outSession = jschan.streamSession(outStream, inStream);
+    cb(null, inSession, outSession);
+  });
+});
+
+describe('stream session with object mode and no headers', function() {
+  abstractSession(function(cb) {
+    var inStream  = new PassThrough({ objectMode: true });
+    var outStream = new PassThrough({ objectMode: true });
+    var inSession = jschan.streamSession(inStream, outStream, { server: true, header: false });
+    var outSession = jschan.streamSession(outStream, inStream, { header: false });
     cb(null, inSession, outSession);
   });
 });
